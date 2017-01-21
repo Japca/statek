9dc65f67da5676dba600062e67e094c0be9bb63f
Jakub <jakub.krhovjak@protonmail.com>
2017-01-21 17:55:32 +0100

    To SPRING BOOT

--- src/main/java/net/statek/controller/WebController.java
+++ src/main/java/net/statek/controller/WebController.java
@@ -5,14 +5,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class WebController {
 
-	@RequestMapping(path ="/", method = RequestMethod.GET)
-	public String index(Model model, @RequestParam(value = "name", required = false) String name) {
-		model.addAttribute("name", name);
-		return "index";
+	@RequestMapping(value ="/statek", method = RequestMethod.GET)
+	public String index(Model model) {
+		return "statek";
+		//new ModelAndView("error.html");
 	}
 
 }
--- src/main/webapp/WEB-INF/jsp/index.jsp
+++
@@ -1,6 +0,0 @@
-
-<jsp:directive.include file="header.jsp" />
-<h1>Test </h1>
-<jsp:directive.include file="footer.jsp" />
-
-
